8f0c6b059b020428d37c34bb7c680296a3e75fde
Miles Burke <milesj76@gmail.com>
2023-08-19 14:14:49 -0400

    add stuff to nav

--- portfolio-v3/src/routes/+layout.svelte
+++ portfolio-v3/src/routes/+layout.svelte
@@ -6,8 +6,11 @@
 </script>
 
 <div>
-  <nav>
-    <div>Logo</div>
+  <nav class="flex justify-between items-center px-6 py-4">
+    <div class="flex items-baseline space-x-4">
+      <h1 class="text-3xl font-bold">mB</h1>
+      <p class="tracking-tighter font-light text-base"><span class="font-bold text-3xl">m</span>iles <span class="font-bold text-3xl">B</span>urke</p>
+    </div>
 
     <!-- Non-Burger Nav -->
     <!-- <div class="hidden flex">
@@ -21,12 +24,12 @@
     <Sheet.Root>
       <Sheet.Trigger asChild let:builder>
         <Button builders={[builder]} variant="ghost" size="icon">
-          <MenuIcon class="h-4 w-4" />
+          <MenuIcon class="h-16 w-16" />
         </Button>
       </Sheet.Trigger>
       <Sheet.Content side="right">
         <Sheet.Header>
-          <Sheet.Title>Miles Burke</Sheet.Title>
+          <!-- <Sheet.Title>Miles Burke</Sheet.Title> -->
           <Sheet.Description>
             Add Social Media Links here
           </Sheet.Description>
@@ -35,10 +38,10 @@
           <a href="#">Home</a>
           <a href="#">Portfolio</a>
           <a href="#">About</a>
-          <a href="#">Articles</a>
+          <!-- <a href="#">Articles</a> -->
         </div>
         <Sheet.Footer>
-          <Button></Button>
+          <Button>Get In Touch</Button>
         </Sheet.Footer>
       </Sheet.Content>
     </Sheet.Root>
