bf09a8b81e1155bad385ece1209ee5191c3af6cc
Miles Burke <milesj76@gmail.com>
2023-08-19 13:59:09 -0400

    add button + setup nav menu

--- portfolio-v3/src/routes/+layout.svelte
+++ portfolio-v3/src/routes/+layout.svelte
@@ -1,5 +1,48 @@
-<script>
+<script lang="ts">
   import "../app.css";
+  import * as Sheet from "$lib/components/ui/sheet";
+  import { Button } from "$lib/components/ui/button";
+  import { MenuIcon } from "lucide-svelte";
 </script>
 
-<slot />
+<div>
+  <nav>
+    <div>Logo</div>
+
+    <!-- Non-Burger Nav -->
+    <!-- <div class="hidden flex">
+      <a href="#">Home</a>
+      <a href="#">Portfolio</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </div> -->
+
+    <!-- Burger Zone -->
+    <Sheet.Root>
+      <Sheet.Trigger asChild let:builder>
+        <Button builders={[builder]} variant="ghost" size="icon">
+          <MenuIcon class="h-4 w-4" />
+        </Button>
+      </Sheet.Trigger>
+      <Sheet.Content side="right">
+        <Sheet.Header>
+          <Sheet.Title>Miles Burke</Sheet.Title>
+          <Sheet.Description>
+            Add Social Media Links here
+          </Sheet.Description>
+        </Sheet.Header>
+        <div class="flex flex-col">
+          <a href="#">Home</a>
+          <a href="#">Portfolio</a>
+          <a href="#">About</a>
+          <a href="#">Articles</a>
+        </div>
+        <Sheet.Footer>
+          <Button></Button>
+        </Sheet.Footer>
+      </Sheet.Content>
+    </Sheet.Root>
+  </nav>
+
+  <slot />
+</div>
--- portfolio-v3/src/routes/+page.svelte
+++ portfolio-v3/src/routes/+page.svelte
@@ -1,9 +1,7 @@
-<h1 class="text-3xl font-bold underline text-red-300">
+<h1 class="text-3xl font-bold underline text-green-700">
   Hello world!
 </h1>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
+
 </style>
