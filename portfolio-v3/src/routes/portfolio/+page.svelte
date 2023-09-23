c69bc8fa64219cf481a1e9286b3a8b4379c26918
Miles Burke <milesj76@gmail.com>
2023-09-23 18:05:25 -0400

    add portfolio page

---
+++ portfolio-v3/src/routes/portfolio/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<div>
+    <header>
+        <h1>Development Portfolio</h1>
+        <p>Examples of the kind of work I've had the great opportunity to work on.</p>
+    </header>
+
+    
+</div>
