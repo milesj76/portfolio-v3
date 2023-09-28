bd8a68e61d0a9d71f133558072401e3e58a627b2
Miles Burke <milesj76@gmail.com>
2023-09-28 09:28:03 -0400

    added content

--- portfolio-v3/src/routes/portfolio/reuse-tech-app/+page.svelte
+++ portfolio-v3/src/routes/portfolio/reuse-tech-app/+page.svelte
@@ -49,6 +49,16 @@
     </article>
 
     <article>
+        <h2>Understanding The System</h2>
+
+        <p>In the process of creating this app, I realized I had to understand how the current system was organized and divided. This would guide me to how I would connect our current data structures together with code. I want to emphasize this part of the project because I found this to be an extremely helpful learning experience. At this point, I hadn't really had the opportunity to work on existing structures and not on such a large and systematic scale. Seeing and organizing the project from macro and micro levels really taught me how to think about code and data structures as a system, how parts of the system interact and what a system requires to start and maintain operation.</p>
+
+        <p>I've since taken this lesson into every project I've worked on since and it has dramatically improved my work on projects, especially those projects of higher complexity. This app will probably always have a special place in my memory.</p>
+
+        <!-- Add image of the system blueprint I made -->
+    </article>
+
+    <article>
         <h2>Improving Inventory Accuracy</h2>
 
         <p>What bothered me most about our inventory was not only how difficult it was to find a computer but how upon opening that computer, it rarely contained the parts it claimed to have. This would actually drive me mad. I would have to physically eyeball every shelf, scanning for a model that would match my order, pull it out and hope that it was in decent enough condition for sale. A little paper tag hung off the front with little pencil writings scarcely explaining what issues were known about the computer. This information was never submitted to our Google Sheet because it occurred to me that the techs felt it wasted too much time figuring out where to type this or how to format it.</p>
@@ -72,5 +82,25 @@
         <h2>Integrating Orders</h2>
 
         <p>Originally, I had only intended to create a highly accurate inventory with an extremely smooth and intuitive experience. As the app progressed towards completion, we techs had the idea to also use the app to simplify working on our orders. This would mean incorporating another Sheet and also virtualizing our order "check list" to provide verification of certain parts of our work. With this, we could automate many manual actions (writing our progress, noting order peripherals to be shipped, etc.) and, in turn, make us more efficient at our work.</p>
+
+        <p>This, again, was a matter of taking a Google Sheet that already existed, integrating it into the AppSheet system and adjusting it to facilitate common functions our technicians would be using. This feature existing alongside the Inventory made the technician's process so much smoother. Now, one could claim an order, its details emphasized in the UI to easily identify and understand, find the related model of computer with the click of a button, see highly accurate information about what was in the computer and where to find it, claim it, and get right to refurbishing.</p>
+
+        <p>On top of this, once it was time to ship the order, we had every order detail to be sure we weren't missing anything and, with a click, add the shipment to our outgoing manifest. The process from start to finish became simpler, less manual and more accurate. All of the main parts of the refurbishing process integrated together to make a vastly simpler experience.</p>
+    </article>
+
+    <article>
+        <h2>Logistical Upgrades</h2>
+
+        <p>Though this wasn't so much code related, it was critical to improving our process. Clearly, the business had always been small and scrappy, running on mostly processes that were made up on the spot under tight time and resource constraints. For example, our inventory shelves all had numbers, but only the entirety of the shelf had a number, not each of its four or five shelves. Now that updating information was simple, we decided make locations more granular by emphasizing shelf (going from shelf "A1" to "A1-1, A1-2, A1-3, etc."). This dramtically reduced search time for computers by reducing the number to look through from 40-50 down to about seven.</p>
+
+        <p>We also decided to try implementing barcodes as this would allow us to search by scan. This seemed like an obvious improvement and with some work and tuning, we had an entire barcode printing system with barcode tagged shelves.</p>
+
+        <p>To my surprise, one logistical issue we had turned out to be a perfect demonstration of data structures and their value. Our smallest computers, known as micros, had a unique form factor that didn't seem to make use of our shelf space efficiently. Because most computers tend to be tall and slender, naturally the shelves would be left tall and allow each computer to sit side by side like a bookshelf. The micros were so short, however, that to place them on the shelves in the same fashion would give up almost half of the allocated shelf space. At the time, the solution was to stack the micros to get more out of the space. The only problem was that micros at the bottom of each stack tended to stay on bottom as getting them out was difficult. With the go ahead from my boss, I restructured the shelf space to be shorter in order to unstack these micros and instead lay them on each shelf as books. This gave us all the benfit of finding and grabbing every micro with ease while also maximizing space. It occurred to me afterwards that this was the value of properly structuring data to improve efficiency in algorithmically using said data. Of course, this made me so much more excited to learn about programming and I believe to be a great way to help someone understand the value and utility of data structures.</p>
+    </article>
+
+    <article>
+        <h2>The Working Product</h2>
+        
+        <p></p>
     </article>
 </div>
