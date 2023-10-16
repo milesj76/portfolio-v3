42bac509166255d206793773dc4dfae7b92412fa
Miles Burke <milesj76@gmail.com>
2023-10-15 22:51:25 -0400

    Add about section

--- portfolio-v3/src/routes/about/+page.svelte
+++ portfolio-v3/src/routes/about/+page.svelte
@@ -1,3 +1,40 @@
-<div>
-  <header>About Miles Burke</header>
+<div class="flex flex-col justify-center w-full mx-auto my-6 p-4 max-w-3xl 2xl:max-w-[80rem] space-y-8">
+  <!-- Title -->
+  <header class="text-4xl font-bold text-center">About Miles Burke</header>
+
+  <article>
+    <p>I am a developer with 2 years of professional experience. I build websites, apps and API's to make business better.</p>
+  </article>
+
+  <article>
+    <p>Most of my experience is in developing web technologies, front-end and back-end. My goal is always to build something that works well and fulfills business needs effectively. While solving code type problems is my main interest, I also enjoy trying to improve how end users will see and use my work. It's great to not only have strong programmatic solutions but to have an organized and understandable presentation that helps guide the user. It's been quite fun to learn about the many problems that need to be solved when creating all kinds of tech.</p>
+  </article>
+
+  <article>
+    <p>Most of my work has been created using JavaScript, Node.js and different frameworks including React and Svelte. By far, I've enjoyed using Svelte and really hope it becomes more widely adopted. Sveltekit and Next.js have been great additions to my toolkit. I love to use Tailwindcss to speed up the styling process.</p>
+  </article>
+
+  <article>
+    <p>It has been an endless challenge as a self-taught developer over the past three or four years to learn to develop software. What I've been most eager about lately is collaborating on larger projects with developers of a higher level. I'm certainly proud that I've managed to break through the frustrations of getting stuck on projects by myself. What I want to grow myself further is to see more of how others work and how I can better improve at building things that are easier to maintain over time.</p>
+  </article>
 </div>
+
+
+<style>
+  article {
+      @apply space-y-6;
+  }
+
+  article > h2 {
+      @apply text-2xl font-semibold;
+  }
+
+  article > p {
+      /* @apply font-serif; */
+      @apply tracking-wide leading-relaxed;
+  }
+
+  .special-box {
+      @apply bg-white-200 p-8 rounded-md shadow-sm;
+  }
+</style>
